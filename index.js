3e03838629153008e955faaec86e21c6293281ee
daeix <110390090+daeix@users.noreply.github.com>
2023-06-19 21:24:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,24 @@ document.getElementById("btn").addEventListener("click", function convertAll() {
   calcThree.textContent = "";
   for (let i = 0; i < 1; i++) {
     let feet = convAll * 3.281;
-    calcOne.textContent += Math.round(feet * 1000) / 1000;
+    let meters = convAll / 3.281;
+    calcOne.textContent += `${convAll} meters = ${feet.toFixed(
+      3
+    )} feet | ${convAll} feet = ${meters.toFixed(3)} meters `;
   }
   for (let i = 0; i < 1; i++) {
     let gallons = convAll * 0.264;
-    calcTwo.textContent += Math.round(gallons * 1000) / 1000;
+    let liters = convAll / 0.264;
+    calcTwo.textContent += `${convAll} gallons = ${gallons.toFixed(
+      3
+    )} liters | ${convAll} liters =  ${liters.toFixed(3)} gallons `;
   }
   for (let i = 0; i < 1; i++) {
     let pounds = convAll * 2.204;
-    calcThree.textContent += Math.round(pounds * 1000) / 1000;
+    let kilograms = convAll / 2.204;
+
+    calcThree.textContent += `${convAll} kilograms = ${pounds.toFixed(
+      3
+    )} pounds | ${convAll} pounds =  ${kilograms.toFixed(3)} kilograms `;
   }
 });
